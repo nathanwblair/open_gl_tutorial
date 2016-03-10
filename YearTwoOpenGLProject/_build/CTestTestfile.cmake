3fd781b815f46db0079bc50cc1100889d603f82d
nathanwblair <nathan.w.blair@gmail.com>
2016-03-10 17:38:20 +1100

    Deleted temp stuff

--- YearTwoOpenGLProject/_build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nathaniel.blair/Documents/GitHub/open_gl_tutorial/YearTwoOpenGLProject
-# Build directory: C:/Users/nathaniel.blair/Documents/GitHub/open_gl_tutorial/YearTwoOpenGLProject/_Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(OpenGLEngine)
-subdirs(GridTutorial)
